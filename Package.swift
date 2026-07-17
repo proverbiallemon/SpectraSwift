6f145237eed5284a464c90ea49b18e2c9f1a8e06
Pocket Bear (Lemon) <robert.mychal.martinez@gmail.com>
2026-07-17 02:27:04 -0400

    Declare iOS support for SpectraKit

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "SpectraKit",
-    platforms: [.macOS(.v14)],
+    platforms: [.macOS(.v14), .iOS(.v16)],
     products: [
         .library(name: "SpectraKit", targets: ["SpectraKit"])
     ],
